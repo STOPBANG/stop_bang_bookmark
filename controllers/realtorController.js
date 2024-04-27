6605602d7be12afdb196ad1f13f0c4f27ecbe46a
plum-king <jeesoo0908@gmail.com>
2024-04-27 22:43:44 +0900

    feat: bookmark db 연동 진행 중

--- controllers/realtorController.js
+++ controllers/realtorController.js
@@ -6,17 +6,12 @@ const { httpRequest } = require('../utils/httpRequest.js');
 
 module.exports = {
   updateBookmark: (req, res) => {
-    const decoded = jwt.verify(
-      req.cookies.authToken,
-      process.env.JWT_SECRET_KEY
-    );
-    const r_username = decoded.userId;
-
+    const r_username = req.body.userId;
     // [start] 북마크 저장하기
     postOptions = {
       host: 'stop_bang_sub_feature_DB',
       port: process.env.PORT,
-      path: `/db/agent/update`,
+      path: `/db/bookmark/update`,
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -26,14 +21,13 @@ module.exports = {
       r_username: r_username,
       raRegno: req.params.ra_regno
     }
+
     httpRequest(postOptions, requestBody)
     .then(result => {
       if (result === null) {
         console.log("error occured: ", err);
       } else {
-        res.json({
-          update: result.body[0]
-        });
+        return res.json({}); // db 수정 다시
       }
     });
     // realtorModel.updateBookmark(r_username, body, (result, err) => {
