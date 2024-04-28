d6840bd27926474648c94ff9e99c8854aae7e092
plum-king <jeesoo0908@gmail.com>
2024-04-29 02:33:03 +0900

    feat: r_id 조회 추가

--- controllers/realtorController.js
+++ controllers/realtorController.js
@@ -5,8 +5,25 @@ const jwt = require("jsonwebtoken");
 const { httpRequest } = require('../utils/httpRequest.js');
 
 module.exports = {
-  updateBookmark: (req, res) => {
+  updateBookmark: async (req, res) => {
     const r_username = req.body.userId;
+        
+    // [start] 로그인 계정 r_id 가져오기
+    getOptions = {
+      host: 'stop_bang_auth_DB',
+      port: process.env.PORT,
+      path: `/db/resident/findById`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }
+    let requestBody = {username: r_username};
+    result = await httpRequest(getOptions, requestBody);
+    const r_id = result.body[0].r_id;
+    console.log(r_id);
+    // [end] 로그인 계정 정보 가져오기
+
     // [start] 북마크 저장하기
     postOptions = {
       host: 'stop_bang_sub_feature_DB',
@@ -17,8 +34,8 @@ module.exports = {
         'Content-Type': 'application/json',
       }
     }
-    const requestBody = {
-      r_id: r_username,
+    requestBody = {
+      r_id: r_id,
       ra_regno: req.params.ra_regno
     }
 
@@ -30,5 +47,6 @@ module.exports = {
         res.redirect(`/realtor/${req.params.ra_regno}`);
       }
     });
+    // [end] 북마크 저장하기
   }
 };
