79f41d40fb19d257056851756b04b2b02906ded8
plum-king <jeesoo0908@gmail.com>
2024-04-29 02:51:07 +0900

    feat: r_id 조회 경로 수정

--- controllers/realtorController.js
+++ controllers/realtorController.js
@@ -20,8 +20,7 @@ module.exports = {
     }
     let requestBody = {username: r_username};
     result = await httpRequest(postOptions, requestBody);
-    console.log(result);
-    const r_id = result.body.r_id;
+    const r_id = result.body[0].r_id;
     // [end] 로그인 계정 정보 가져오기
 
     // [start] 북마크 저장하기
