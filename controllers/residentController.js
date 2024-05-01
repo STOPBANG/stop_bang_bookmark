b3372331802c9c52ddcc4a813ec27860876bd41d
joiejuni <lumiere.298@gmail.com>
2024-05-02 05:05:34 +0900

    fix: 안 쓰는 함수 삭제

--- controllers/residentController.js
+++ controllers/residentController.js
@@ -38,40 +38,6 @@ module.exports = {
         });
         req.pipe(forwardRequest);
       },
-      openReview: (req, res) => {
-        /* msa */
-        const getOptions = {
-          host: 'stop_bang_resident_mypage',
-          port: process.env.MS_PORT,
-          path: '/openreview',
-          method: 'GET',
-          headers: {
-            ...
-            req.headers,
-            auth: res.locals.auth
-          }
-        }
-        const forwardRequest = http.request(
-          getOptions,
-          forwardResponse => {
-            let data = '';
-            forwardResponse.on('data', chunk => {
-              data += chunk;
-            });
-            forwardResponse.on('end', () => {
-              return res.render("resident/openReview", JSON.parse(data));
-            });
-          }
-        );
-        forwardRequest.on('close', () => {
-          console.log('Sent [openreview] message to resident_mypage microservice.');
-        });
-        forwardRequest.on('error', (err) => {
-          console.log('Failed to send [openreview] message');
-          console.log(err && err.stack || err);
-        });
-        req.pipe(forwardRequest);
-      },
 
 
 };
