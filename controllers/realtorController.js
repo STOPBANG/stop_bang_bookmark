74ff689b2b14682996a5fe9a5caf16f33b34fa7f
plum-king <jeesoo0908@gmail.com>
2024-05-26 21:43:18 +0900

    feat: bookmark 삭제 추가

--- controllers/realtorController.js
+++ controllers/realtorController.js
@@ -4,7 +4,7 @@ const { httpRequest } = require('../utils/httpRequest.js');
 
 module.exports = {
   updateBookmark: async (req, res) => {
-    const r_username = req.body.userId;
+    const r_username = req.body.r_username;
     
     // [start] 로그인 계정 r_id 가져오기
     postOptions = {
@@ -21,29 +21,70 @@ module.exports = {
     const r_id = result.body[0].id;
     // [end] 로그인 계정 정보 가져오기
 
-    // [start] 북마크 저장하기
-    postOptions = {
+    // [start] 북마크 조회
+    const getBookmarkOptions = {
       host: 'stop_bang_sub_DB',
       port: process.env.PORT,
-      path: `/db/bookmark/create`,
-      method: 'POST',
+      path: `/db/bookmark/findALLByIdnRegno/${r_id}/${req.params.sys_regno}`,
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       }
     }
-    requestBody = {
-      r_id: r_id,
-      ra_regno: req.params.ra_regno
-    }
-
-    httpRequest(postOptions, requestBody)
-    .then(result => {
-      if (result === null) {
-        console.log("error occured: ", err);
-      } else {
-        res.redirect(`/realtor/${req.params.ra_regno}`);
+    httpRequest(getBookmarkOptions)
+    .then(getBookmarkResult => {
+       // [end] 북마크 조회
+       if(getBookmarkResult.length){ // 북마크가 있으면
+        // [start] 북마크 삭제
+        postBookmarkDeleteOptions = {
+          host: 'stop_bang_sub_DB',
+          port: process.env.PORT,
+          path: `/db/bookmark/delete`,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        }
+        requestBody = {
+          r_id: r_id,
+          sys_regno: req.params.sys_regno
+        }
+        httpRequest(postOptions, requestBody)
+        .then(result => {
+          if (result === null) {
+            console.log("error occured: ", err);
+          } else {
+            return res.redirect(`/realtor/${req.params.sys_regno}`);
+          }
+        });
+        // [end] 북마크 삭제
       }
-    });
-    // [end] 북마크 저장하기
-  }
+      else{ // 북마크가 없으면 추가
+        // [start] 북마크 추가
+        postOptions = {
+          host: 'stop_bang_sub_DB',
+          port: process.env.PORT,
+          path: `/db/bookmark/create`,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        }
+        requestBody = {
+          r_id: r_id,
+          sys_regno: req.params.sys_regno
+        }
+    
+        httpRequest(postOptions, requestBody)
+        .then(result => {
+          if (result === null) {
+            console.log("error occured: ", err);
+          } else {
+            return res.redirect(`/realtor/${req.params.sys_regno}`);
+          }
+        });
+        // [end] 북마크 추가
+      }
+    })
+  }  
 };
